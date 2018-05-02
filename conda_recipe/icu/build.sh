977a1d2f7cac162dbec7bd34477cf430acf7b54b
domosute <domosute@users.noreply.github.com>
2018-05-02 18:45:04 -0500

    Create build.sh

---
+++ conda_recipe/icu/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd source
+chmod +x configure install-sh
+
+if [ "$(uname)" == "Linux" ]; then
+    export CC=gcc
+    export CXX=g++
+
+    ./configure --prefix="$PREFIX"
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    export LANG=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
+    export LDFLAGS="-Wl,-headerpad_max_install_names"
+
+    ./configure --prefix="$PREFIX" \
+        --disable-samples \
+        --disable-tests \
+        --enable-static \
+        --with-library-bits=64
+fi
+
+make
+make install
