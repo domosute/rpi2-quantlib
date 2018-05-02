c22dc5b83f56d8902e5de70b121a4274d42756bb
domosute <domosute@users.noreply.github.com>
2018-05-02 18:49:03 -0500

    Create build.sh

---
+++ conda_recipe/quantlib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PREFIX=/opt/conda # Adjust accordingly
+
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+./autogen.sh
+./configure --prefix=$PREFIX
+make -j 4 -C Python
+sudo make -C Python install
