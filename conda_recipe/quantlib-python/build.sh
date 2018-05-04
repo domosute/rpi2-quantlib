62bd0634df49fac2b6b92d2ae544da0752a2404d
domosute <domosute@users.noreply.github.com>
2018-05-04 13:40:59 -0500

    Update build.sh

--- conda_recipe/quantlib-python/build.sh
+++ conda_recipe/quantlib-python/build.sh
@@ -7,5 +7,6 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 ./autogen.sh
 ./configure --prefix=$PREFIX
-make -C Python 
+# make -j 4 -C Pyhton  # This works for x86_64
+make -C Python # Multi-Threading process caused memory error under RPi3 B+; thus, set to single thread.
 sudo make -C Python install
