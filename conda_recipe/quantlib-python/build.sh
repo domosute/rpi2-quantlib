a6a9246ca5220c77915cb04e6843b82d340b54c8
domosute <domosute@users.noreply.github.com>
2018-05-04 13:34:30 -0500

    Update build.sh

--- conda_recipe/quantlib-python/build.sh
+++ conda_recipe/quantlib-python/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-MULTI_THREAD=2 # Adjust it to less if memory error occurs during build.
-
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
@@ -9,5 +7,5 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 ./autogen.sh
 ./configure --prefix=$PREFIX
-make -j ${MULTI_THREAD} -C Python 
+make -C Python 
 sudo make -C Python install
