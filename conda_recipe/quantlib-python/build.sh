b9f3c2937d04300117f398db1267bc9f1b24f476
domosute <domosute@users.noreply.github.com>
2018-05-04 13:14:21 -0500

    Update build.sh

--- conda_recipe/quantlib-python/build.sh
+++ conda_recipe/quantlib-python/build.sh
@@ -7,5 +7,5 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 ./autogen.sh
 ./configure --prefix=$PREFIX
-make -j 2 -C Python # Adjust CPU_COUNT -j option if memory error occurs during gcc compilation.
+make -j 2 -C Python # Adjust "-j CPU_COUNT" if memory error occurs during gcc compilation.
 sudo make -C Python install
