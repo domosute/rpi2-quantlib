0b84285b57dc7e67330636f7a459dc268309db28
domosute <domosute@users.noreply.github.com>
2020-07-21 10:00:28 -0500

    Update build.sh

--- conda_recipe/quantlib-python/build.sh
+++ conda_recipe/quantlib-python/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PREFIX=/opt/conda
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
@@ -8,8 +9,8 @@ export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 ./autogen.sh
 ./configure --prefix=$PREFIX
 # make -j 4 -C Pyhton  # This works for x86_64
-# make -C Python # Multi-Threading process caused memory error under RPi3 B+; thus, set to single thread.
-# sudo make -C Python install
+make -C Python # Multi-Threading process caused memory error under RPi3 B+; thus, set to single thread.
+sudo make -C Python install
 # Use setup.py if building is done from a Git checkout
 # Reference: https://github.com/lballabio/QuantLib-SWIG/tree/master/Python
 cd Python
